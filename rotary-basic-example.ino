34e9889c16c4dae925624859810cd8aa8e080d68
Andrew Brown <algorithmicmusic@gmail.com>
2024-12-31 16:42:20 +1000

    Added counter to example

--- rotary-basic-example.ino
+++ rotary-basic-example.ino
@@ -2,6 +2,8 @@
 
 #include "rotary_basic.h"
 
+int counter = 0;
+
 void setup() {
   Serial.begin(115200);
   // pins A, B, and Push
@@ -10,8 +12,17 @@ void setup() {
 
 void loop() {
   int state = updateRotaryBasic(); // needs rapid updating
-  if (state == 1) Serial.println("CCW");
-  if (state == 2) Serial.println("CW");
-  if (state == 3) Serial.println("PUSH DOWN");
+  if (state == 1) {
+    counter--;
+    Serial.println(String(counter) + " CCW");
+  }
+  if (state == 2) {
+    counter++;
+    Serial.println(String(counter) + " CW");
+  }
+  if (state == 3) {
+    counter = 0;
+    Serial.println(String(counter) + " PUSH DOWN");
+  }
   if (state == 4) Serial.println("PUSH UP");
 }
