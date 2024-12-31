eed34a095fd960f5dc241185e219db6bbad8c2aa
Andrew Brown <algorithmicmusic@gmail.com>
2024-12-31 13:35:08 +1000

    Updated for ESP32

--- rotary-basic-example.ino
+++ rotary-basic-example.ino
@@ -5,12 +5,13 @@
 void setup() {
   Serial.begin(115200);
   // pins A, B, and Push
-  setupRotaryBasic(16, 0, 13);
+  setupRotaryBasic(33, 37, 38);
 }
 
 void loop() {
-    int state = updateRotaryBasic();
-    if (state == 1) Serial.println("CCW");
-    if (state == 2) Serial.println("CW");
-    if (state == 3) Serial.println("PUSH");
+  int state = updateRotaryBasic(); // needs rapid updating
+  if (state == 1) Serial.println("CCW");
+  if (state == 2) Serial.println("CW");
+  if (state == 3) Serial.println("PUSH DOWN");
+  if (state == 4) Serial.println("PUSH UP");
 }
--- rotary_basic.h
+++ rotary_basic.h
@@ -7,28 +7,39 @@ void setupRotaryBasic(int a, int b, int p) {
   rotaryPins[0] = a;
   rotaryPins[1] = b;
   rotaryPins[2] = p;
+  pinMode(rotaryPins[0], INPUT_PULLUP);
+  pinMode(rotaryPins[1], INPUT_PULLUP);
   pinMode(rotaryPins[2], INPUT_PULLUP);
 }
 
 int updateRotaryBasic() {
-  int state = 0; // 0= off, 1 = CW, 2 = CCW, 3 = Push
-  if (millis() - potTime > 2) {
+  int state = 0; // 0= off, 1 = CW, 2 = CCW, 3 = Push down, 4 = push up
+  if (millis() - potTime > 2) { 
     potTime = millis();
     int pinA = digitalRead(rotaryPins[0]);
     int pinB = digitalRead(rotaryPins[1]);
     int pinPush = digitalRead(rotaryPins[2]);
+
+    // if (pinA != prevA || pinB != prevB || pinPush != prevP) {
+    // if (pinA == 0 || pinB == 0 || pinPush == 0) {
+    //   Serial.print(pinA);Serial.print(" ("); Serial.print(prevA);Serial.print(") ");
+    //   Serial.print(pinB);Serial.print(" ");Serial.print(" ("); Serial.print(prevB);Serial.print(") ");
+    //   Serial.print(pinPush);Serial.print(" ("); Serial.print(prevP);Serial.println(") ");
+    // }
     
-//     Serial.print(pinA);Serial.print(" "); Serial.print(pinB);Serial.print(" "); Serial.println(pinPush);
-    
-    if (pinA == 1 && pinB == 1 && prevA == 0 && state == 0 && prevB == 0) {
-//      Serial.print("CCW "); Serial.print(pinA); Serial.print(" "); Serial.print(pinB);Serial.print(" "); Serial.print(prevA); Serial.print(" "); Serial.println(prevB);
+    if (pinA == 1 && prevA == 0 && pinB == 0  && state != 1) {
+    //  Serial.print("CCW "); Serial.print(pinA); Serial.print(" "); Serial.print(pinB);Serial.print(" "); Serial.print(prevA); Serial.print(" "); Serial.println(prevB);
       state = 1;
-    } else if (pinA == 0 && pinB == 0 && prevA == 1) {
-//      Serial.print("CW "); Serial.print(pinA); Serial.print(" "); Serial.print(pinB);Serial.print(" ");Serial.print(prevA); Serial.print(" "); Serial.println(prevB);
+    } else if (pinA == 0 && pinB == 1 && prevB == 0 && state != 2) {
+    //  Serial.print("CW "); Serial.print(pinA); Serial.print(" "); Serial.print(pinB);Serial.print(" ");Serial.print(prevA); Serial.print(" "); Serial.println(prevB);
       state = 2;  
     } else if (pinPush == 0 && state != 3 && prevP == 1 ) {
-//      Serial.print("Push "); Serial.println(pinPush);
+          //  Serial.print("Push down"); Serial.println(pinPush);
       state = 3;
+    // } else if (pinPush == 1 && state == 3 && prevP == 0 ) {
+    } else if (pinPush == 1 && prevP == 0 ) {
+      state = 4;
+      //  Serial.print("Push up"); Serial.println(pinPush);
     }
     
     prevA = pinA;
